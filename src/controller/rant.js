1b035f821c6b8bad99b7c45fdf679f8e00bb2f35
Pietro Sousa de Oliveira <75648885+pietrooliveira17@users.noreply.github.com>
2025-05-08 08:31:31 -0300

    Add files via upload

---
+++ src/controller/rant.js
@@ -0,0 +1,27 @@
+const RantRepository = require('../model/rantModel');
+const bcryptjs = require('bcryptjs');
+const sha1 = require('sha1');
+let n = ''
+let bcryptInp = '';
+let sha1Inp = ''
+let scrollTo = ''
+
+
+function main(req, res){
+    res.render("rant", { valor: n, bcrypt: bcryptInp, sha1: sha1Inp, scrollTo: scrollTo });
+}
+
+async function geraHash(req, res){
+    bcryptInp = bcryptjs.hashSync(req.body.nome);
+    sha1Inp = sha1(req.body.nome);
+    scrollTo = true;
+    n = req.body.nome
+    await RantRepository.create({
+        valorInput: req.body.nome,
+        bcrypt: bcryptInp,
+        sha1: sha1Inp,
+    });
+    res.redirect("/rant#tabel-hash");
+    
+}
+module.exports = { main, geraHash }
